6688c58f1dbc5c97724d30eec0ef7773723396f1
Sean Hellum <seanhellum45@gmail.com>
2020-03-20 13:30:05 +0000

    update site

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -3,15 +3,19 @@
 extern crate rocket;
 use rocket::response::NamedFile;
 use std::io;
-use std::path::{Path, PathBuf};
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("site/index.html")
 }
 
+#[get("/style.css")]
+fn get_style() -> io::Result<NamedFile> {
+    NamedFile::open("site/style.css")
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, get_style])
         .launch();
 }
