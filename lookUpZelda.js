f33bab6006a2bb0778a10bfc5e32172542261011
Sean Hellum <seanhellum45@gmail.com>
2020-03-29 13:20:34 +0000

    screenshot

--- lookUpZelda.js
+++ lookUpZelda.js
@@ -1,20 +1,22 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false})
-  const page = await browser.newPage()
-  
-  const navigationPromise = page.waitForNavigation()
-  
-  await navigationPromise
-  
-  await page.goto('https://www.google.com/')
-  
-  await page.setViewport({ width: 1366, height: 641 })
-  
-  await page.waitForSelector('.A8SBwf > .RNNXgb > .SDkEP > .a4bIc > .gLFyf')
-  await page.click('.A8SBwf > .RNNXgb > .SDkEP > .a4bIc > .gLFyf')
-  
-  await navigationPromise
-  
-  await browser.close()
-})()
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.google.com/");
+
+  await page.focus("input[name=q]");
+
+  await page.keyboard.type("Zelda");
+
+  await page.keyboard.press("Enter");
+
+  await page.waitForNavigation();
+
+  await page.screenshot({ path: "ex.png" });
+
+  await browser.close();
+})();
