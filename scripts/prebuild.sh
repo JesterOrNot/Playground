303e174918e52a64062c4947f8db41e10bbbdad0
Sean Hellum <seanhellum45@gmail.com>
2020-04-24 23:53:11 +0000

    update

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -5,5 +5,6 @@ program_name="${ADDR[1]}"
 program_name=${program_name//'"'/""}
 nohup cargo run &
 gp await-port 8000
+echo "Killing process"
 kill $(pidof target/debug/$program_name)
 echo "command `cargo run` successfully cached"
