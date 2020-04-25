6475748b62417de70207937837b6550b20aa7ec6
Sean Hellum <seanhellum45@gmail.com>
2020-04-25 00:05:37 +0000

    Stuffs

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,10 +1,13 @@
+echo "Starting..."
 IFS=" = "
 str=$(awk -F: '/^name/' Cargo.toml)
 read -ra ADDR <<< "$str"
 program_name="${ADDR[1]}"
 program_name=${program_name//'"'/""}
+echo "Crate name: $program_name"
 nohup cargo run &
 gp await-port 8000
 echo "Killing process"
-kill $(pidof target/debug/$program_name)
+echo "Process: $(pidof target/debug/$program_name)"
+killall -9 $(pidof target/debug/$program_name)
 echo "command `cargo run` successfully cached"
