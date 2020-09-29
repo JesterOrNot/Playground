ec83b3b9fe2fc4d4a8247031261c1ad85ec4adc8
Sean Hellum <seanhellum45@gmail.com>
2020-09-29 14:40:10 -0500

    Update trie.py

--- trie.py
+++ trie.py
@@ -2,8 +2,9 @@ from pprint import pformat, pprint
 
 
 class TrieNode:
-    def __init__(self, isComplete=False):
+    def __init__(self, value, isComplete=False):
         self.children = {}
+        self.value = value
         self.isComplete = isComplete
 
     def __repr__(self):
@@ -13,19 +14,27 @@ class TrieNode:
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode("")
 
+
+    def add(self, word):
+        current = self.root
+        for i, char in enumerate(word):
+            if char not in current.children:
+                prefix = word[0:i+1]
+                current.children[char] = TrieNode(prefix)
+            current = current.children[char]
+            
     def insert(self, word):
         word = word.lower()
         current_node = self.root
-        index = 0
         length = len(word) - 1
-        for char in word:
+        for index, char in enumerate(word):
             if char not in current_node.children:
-                current_node.children[char] = TrieNode(
-                ) if index != length else TrieNode(True)
+                prefix = word[0:index+1]
+                current_node.children[char] = TrieNode(prefix
+                ) if index != length else TrieNode(prefix, True)
             current_node = current_node.children[char]
-            index += 1
     
     def get_prefix(self, prefix):
         prefix_exists = False
@@ -42,6 +51,21 @@ class Trie:
             current_node = current_node.children[char]
             index += 1
         return (current_node, prefix_exists)
+        
+
+
+    def get_words(self, prefix):
+        prefix = prefix.lower()
+        node, exists = self.get_prefix(prefix)
+        words = []
+        self.get_words_from_node(node, words)
+        return words
+
+    def get_words_from_node(self, node, words):
+        if node.isComplete:
+            words.append(node.value)
+        for char in node.children:
+            self.get_words_from_node(node.children[char], words)
 
 
 if __name__ == "__main__":
@@ -52,3 +76,4 @@ if __name__ == "__main__":
     trie.insert("Ape")
     trie.insert("Soda")
     pprint(trie.root.children)
+    pprint(trie.get_words("Ap"))
